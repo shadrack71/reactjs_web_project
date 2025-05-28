abc853514d8a6796031e5eded2cb876ed34697f0
shadrack <shadrackkinsimba@gmail.com>
2025-05-28 14:47:22 +0300

    working  with events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,37 @@ const content = () => {
         let rand_number = Math.floor(Math.random()*3)
         return names[rand_number]
       }
+      const handleClick = () =>{
+        console.log('you clicked it ')
+
+      }
+      const handleClick2 = (name) =>{
+        console.log(`${name} was clicked` )
+
+      }
+      const handleClick3 = (e) =>{
+        console.log(e.target)
+
+      }
   return (
     <main>
         <p>
             {name_functions()}
+
         </p>
+        <button onDoubleClick={handleClick}>
+            check it1
+
+        </button><br/>
+        <button onClick={()=> handleClick2('attaboy43')}>
+            check it2
+
+        </button>
+        <button onClick={(e)=> handleClick3(e)}>
+            check it3
+
+        </button>
+
     </main>
   )
 }
