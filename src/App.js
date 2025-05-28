1a0c151aae68ab1d30df2a9959f7a692736a5c7f
shadrack <shadrackkinsimba@gmail.com>
2025-05-28 15:36:33 +0300

    remove unused files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from './Footer';
 
 function App() {
   
-  
   return (
     <div className="App">
       <Header />
--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ const Content = () => {
     const [name , setName] = useState('shadrack attaboy')
     const [count , setCount] = useState(0)
     
-    const name_functions =() =>{
+    const name_functions =() => {
         const names = [ 'shadrack','kinsimba','kabazo']
         let rand_number = Math.floor(Math.random()*3)
         setName( names[rand_number])
@@ -16,16 +16,16 @@ const Content = () => {
         console.log(count)
         let i = count +1
         setCount(i)
-}
+    }
       const handleClick2 = (name) =>{
         console.log(`${name} was clicked` )
 
       }
+
       const handleClick3 = () =>{
         let i = count -1
         setCount(i)
-
-      }
+    }
   return (
     <main>
         <p> Hello , {name}</p>
@@ -33,7 +33,7 @@ const Content = () => {
         <button onClick={name_functions}>change name</button><br/>
         <button onClick={handleClick}>add count</button><br/>
         <button onClick={handleClick3}>sub count</button>
-        <button onClick={(e)=> handleClick3(e)}>check it3</button>
+        <button onClick={(e)=> handleClick2(e)}>check it3</button>
 
     </main>
   )
