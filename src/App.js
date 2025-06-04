8df4e9f9b98c7f00cd84f9b00a05f47c5607ca09
shadrack <shadrackkinsimba@gmail.com>
2025-06-04 23:46:03 +0300

    feat: adding controlled  inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react'
@@ -6,33 +7,37 @@ import { useState } from 'react'
 function App() {
 
 
-  const [ items , setItems] = useState([
-    {
-        id: 1,
-        checked: true,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-]);
-const handleCheck = (id)=>{
-  const listItems = items.map((item)=> item.id === id ? { ...item, checked: !item.checked} : item)
+  const [ items , setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+const [newItem ,setNewItem] = useState('')
+const setAndSave = (listItems)=>{
   setItems(listItems)
   localStorage.setItem('shoppinglist',JSON.stringify(listItems))
 
 }
+const addItem = (item) =>{
+  const id = items.length ? items[items.length -1].id +1 :1 
+  const myNewItem = { id ,checked:false,item}
+  const listItems = [...items , myNewItem]
+  setAndSave(listItems)
+}
+
+const handleCheck = (id)=>{
+  const listItems = items.map((item)=> item.id === id ? { ...item, checked: !item.checked} : item)
+  setAndSave(listItems)
+
+}
 const handleDelete = (id) =>{
   const listItems = items.filter((item) =>item.id !== id)
-  setItems(listItems)
-  localStorage.setItem('shoppinglist',JSON.stringify(listItems))
+  setAndSave(listItems)
+
+}
+
+const handleSubmit = (e)=>{
+  e.preventDefault()
+  if(!newItem) return
+  addItem(newItem)
+  setNewItem('')
 
 }
   
@@ -40,9 +45,13 @@ const handleDelete = (id) =>{
   return (
     <div className="App">
       <Header title = 'Groceries List' />
+      <AddItem
+        newItem = {newItem}
+        setNewItem = {setNewItem}
+        handleSubmit = {handleSubmit}
+      />
       <Content 
       items = {items}
-      
       handleCheck = {handleCheck}
       handleDelete ={handleDelete}
       />
--- src/ItemList.js
+++ src/ItemList.js
@@ -16,9 +16,6 @@ const ItemList = ({items,handleCheck,handleDelete}) => {
     ))}
   </ul>
 
-
-
-
   )
 }
 
